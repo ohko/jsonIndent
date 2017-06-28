5efe7c043659f9e67ba23620425b005c7eb9306a
ohko <ohko@qq.com>
2017-06-28 11:48:42 +0800

    增加pipe的支持

--- main.go
+++ main.go
@@ -1,25 +1,64 @@
 package main
 
 import (
-	"encoding/json"
+	"bufio"
+	"errors"
 	"fmt"
 	"log"
 	"os"
+
+	jsoniter "github.com/json-iterator/go"
 )
 
 func main() {
+	c, e1 := pipe()
+	if !c && e1 != nil {
+		fmt.Println(e1)
+		os.Exit(1)
+	}
+	if !c {
+		os.Exit(0)
+	}
+	cmd()
+}
+
+func pipe() (bool, error) {
+	fileInfo, _ := os.Stdin.Stat()
+	if (fileInfo.Mode() & os.ModeNamedPipe) != os.ModeNamedPipe {
+		return true, errors.New("no pipe")
+	}
+	s := bufio.NewScanner(os.Stdin)
+	for s.Scan() {
+		j := jsoniter.Config{IndentionStep: 2}.Froze()
+		var js interface{}
+		if err := j.UnmarshalFromString(s.Text(), &js); err != nil {
+			return false, err
+		}
+
+		indent, err := j.MarshalToString(js)
+		if err != nil {
+			return false, err
+		}
+		fmt.Println(indent)
+	}
+	return false, nil
+}
+
+func cmd() {
 	if len(os.Args) < 2 {
 		log.Println("Eg: ", os.Args[0], "'{a:1,b:2}'")
 		os.Exit(1)
 	}
 
+	j := jsoniter.Config{IndentionStep: 2}.Froze()
 	var js interface{}
-	json.Unmarshal([]byte(os.Args[1]), &js)
-
-	indent, err := json.MarshalIndent(js, "", "  ")
-	if err != nil {
-		log.Println(err)
-		os.Exit(1)
+	if err := j.UnmarshalFromString(os.Args[1], &js); err != nil {
+		log.Fatalln(err)
 	}
-	fmt.Println(string(indent))
+
+	indent, err := j.MarshalToString(js)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	fmt.Println(indent)
 }
